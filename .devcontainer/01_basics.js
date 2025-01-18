b22865da91c2ce919d2d23985266d054eb1c474e
Piyush Namdeo Chandre <161145253+Piyushchandre2004@users.noreply.github.com>
2025-01-18 20:38:46 +0000

    commit changes

--- .devcontainer/01_basics.js
+++ .devcontainer/01_basics.js
@@ -1,1 +1,48 @@
-console.log("Piyush")
+// console.log("Piyush")
+
+// console.log(3+3);
+
+let name="Piyush"       //string
+let age=20              //int
+let isLoggedIn=true     //boolean
+//null => standalone value 
+
+// console.log(typeof "Piyush")
+
+let score=12
+//console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber) // prints NaN = Not A Number
+
+// console.log(Boolean(score))
+
+// let num1,num2,num3
+// num1=num2=num3=2+2
+// console.table({num1,num2,num3})
+
+// console.log(2==2)
+// console.log(2==1)
+// console.log(2!=2)
+// console.log(2>2)
+// console.log(2>1)
+// console.log(2<1)
+
+const myArr=[0,10 ,2,3]
+//for(const i=0;i<3;i++)console.log(myArr[i]);
+
+// myArr.push('piyush')
+// console.log(myArr)
+// myArr.pop()
+// console.log(myArr)
+myArr.unshift(9)
+//push pop  == unshif shift
+
+// console.log(myArr.includes(10))
+
+// myArr.slice(1,3)
+// console.log(myArr.slice(1,3))   splice cuts the array completely
+
+
+
